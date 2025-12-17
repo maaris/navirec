b3e2efa7d0b16a32813ae394262c4d434af013c7
Maris Cilitis <maris.cilitis@gmail.com>
2025-12-17 19:41:45 +0200

    initial commit

---
+++ spec/navirec_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Navirec do
+  it "has a version number" do
+    expect(Navirec::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
